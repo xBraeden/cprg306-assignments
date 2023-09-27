a33256dcaf8f5cc2ae9c76ce8fead75efe8ec06f
xBraeden <120061122+xBraeden@users.noreply.github.com>
2023-09-27 11:26:17 -0600

    latest
    c

--- app/studentinfo.jsx
+++ app/studentinfo.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const StudentInfo = () => {
     return (
         <div>
-            <h1>Braeden Given</h1>
+            <h1>BraedenGiven</h1>
             <h2>Course Section: CPRG-306A</h2>
             <p>
             <Link href="https://github.com/xBraeden">GitHub Link</Link>
